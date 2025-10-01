d2962e1e6faebcbb2631fe138811e605fc3fe510
natalia47256 <nataliap1111@yahoo.com>
2025-10-01 16:04:03 +0200

    Сохраняю изменения из Footer и ScrollToTop перед обновлением из main

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,19 @@
 // frontend/src/App.jsx
 import React from "react";
+import { Route, Routes } from 'react-router-dom'
+import Layout from '@components/Layout/Layout'
+import ScrollToTop from '@components/ScrollToTop/ScrollToTop'
 
 export default function App() {
   return (
     <>
-      <h1>Hello Garden-shop</h1>
+      <h1>Hello Garden-shop!</h1>
+      
+      <ScrollToTop />
+      <Routes>
+        <Route path='/' element={<Layout />}>
+        </Route>
+      </Routes>
     </>
   )
 }
